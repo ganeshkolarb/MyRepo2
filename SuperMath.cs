e02ac4cbadf31d4b1726255a64ebaf7da02e2327
ganeshkolarb <53069710+ganeshkolarb@users.noreply.github.com>
2019-07-19 13:44:14 +0530

    Update SuperMath.cs

--- SuperMath.cs
+++ SuperMath.cs
@@ -2,7 +2,7 @@ public class SuperMath [
  public int Add(int i, int j) {
      return i+j;
 	}
-	public int(int i, int j) {
+	public int product (int i, int j) {
 		return i*j;
 	}
 	public int subtract {int i, intj) {
